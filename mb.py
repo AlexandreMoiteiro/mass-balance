0e8a74f0a68c35e02b1bd1cae25d6cbe17615384
Alexandre Moiteiro <alexandre.moiteiro@gmail.com>
2025-07-14 22:44:44 +0100

    Update mb.py

--- mb.py
+++ mb.py
@@ -33,7 +33,7 @@ class CustomPDF(FPDF):
 
 st.set_page_config(
     page_title="Mass & Balance Planner",
-    page_icon="</>",   # <-- abacus emoji
+    page_icon="u1F5A9",   # <-- abacus emoji
     layout="wide"
 )
 
